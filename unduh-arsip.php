304086e5936fcff45623982913be8016ef72296c
relcarel07 <carelputraqp@gmail.com>
2022-11-08 20:10:56 +0700

    lsp

--- unduh-arsip.php
+++ unduh-arsip.php
@@ -5,6 +5,7 @@ $folder = "file_surat/";
 $filename = $_GET['file'];
 $file_extension = strtolower(substr(strrchr($filename,"."),1));
 // Lalu cek menggunakan fungsi file_exist
+
 if (!file_exists($folder.$_GET['file'])) {
  echo "<h1>Access forbidden!</h1>
  <p>File Sudah Tidak Ada</p>";
